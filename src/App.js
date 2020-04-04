e5e4becba3f1ed4a70a0fe4e8bc07725cdfd4321
Aleksas Grodis <aleksasgrodis@Aleksass-iMac.local>
2020-04-04 17:53:04 +0200

    decided to switch things up and remake the UI with Material UI instead of personal CSS. Who writes their own CSS anymore?. many UI renditions coming, but should look sweet in the end result. input field and button are messed up, will fix in the next commit.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,32 @@
 import React, { useState } from "react";
+import ReactDOM from "react-dom";
 import logo from "./logo.svg";
 import "./App.css";
+import Button from "@material-ui/core/Button";
+import AddBoxOutlinedIcon from "@material-ui/icons/AddBoxOutlined";
+import DeleteIcon from "@material-ui/icons/Delete";
+import IconButton from "@material-ui/core/IconButton";
+import { createStyles, Theme, makeStyles } from "@material-ui/core/styles";
+import SaveIcon from "@material-ui/icons/Save";
+import AddIcon from "@material-ui/icons/Add";
+import TextField from "@material-ui/core/TextField";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemIcon from "@material-ui/core/ListItemIcon";
+import ListItemSecondaryAction from "@material-ui/core/ListItemSecondaryAction";
+import ListItemText from "@material-ui/core/ListItemText";
+import Checkbox from "@material-ui/core/Checkbox";
+import CommentIcon from "@material-ui/icons/Comment";
+import Divider from "@material-ui/core/Divider";
+import Typography from "@material-ui/core/Typography";
+import { typography } from "@material-ui/system";
+import Box from "@material-ui/core/Box";
+import Grid from "@material-ui/core/Grid";
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import ExpansionPanel from '@material-ui/core/ExpansionPanel';
+import ExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary';
+import ExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails';
 
 function App() {
   const [todoList, setTodoList] = useState([
@@ -9,14 +35,14 @@ function App() {
       text: "Clean workdesk",
       completed: false,
       note: "Remove gum from under the desk",
-      date: new Date(0)
+      date: new Date(0),
     },
     { id: 1, text: "Sell organs", completed: false, date: new Date(0) },
     {
       id: 2,
       text: "Delete browsing history",
       completed: false,
-      date: new Date(0)
+      date: new Date(0),
     },
     { id: 3, text: "Buy toilet paper", completed: true, date: new Date(0) },
     { id: 4, text: "Inhale dust cleaner", completed: true, date: new Date(0) },
@@ -26,12 +52,22 @@ function App() {
       text: "Buy private jet",
       completed: true,
       note: "the very expensive one",
-      date: new Date(0)
-    }
+      date: new Date(0),
+    },
   ]);
 
-  // maybe create a single function for both actions?
-  const taskDone = event => {
+  const useStyles = makeStyles((theme) => ({
+    margin: {
+      margin: theme.spacing(1),
+    },
+    extendedIcon: {
+      marginRight: theme.spacing(1),
+    },
+  }));
+
+  const classes = useStyles();
+
+  const taskDone = (event) => {
     event.preventDefault();
     let newList = [...todoList];
     newList[event.target.name].completed = event.target.checked;
@@ -39,103 +75,134 @@ function App() {
     setTodoList(newList);
   };
 
-  // disallow empty todo, add date for sorting
-  const addTodo = event => {
+  const addTodo = (event) => {
     event.preventDefault();
-    if (newTodo.length !== 0){
+    if (newTodo.length !== 0) {
       const newTodos = {
         text: newTodo,
         completed: false,
         id: todoList.length,
-        date: new Date()
+        date: new Date(),
       };
       setTodoList(todoList.concat(newTodos));
       setNewTodo("");
     } else {
-      alert("Task input field may not be empty!")
+      alert("Task input field may not be empty!");
     }
   };
 
-  const finishedTodos = todoList.filter(todo => todo.completed === true);
+  const finishedTodos = todoList.filter((todo) => todo.completed === true);
 
-  const onChangeHandler = event => {
+  const onChangeHandler = (event) => {
     setNewTodo(event.target.value);
   };
 
   const ActiveTasks = () => {
     return todoList
-      .filter(task => task.completed === false)
+      .filter((task) => task.completed === false)
       .sort((a, b) => b.date - a.date)
-      .map(task => (
-        <li key={task.id}>
-          <input
-            type="checkbox"
-            key={task.id}
-            name={task.id}
-            checked={task.completed}
-            onChange={taskDone}
-            id={"box" + task.id}
-          />
-          <label htmlFor={"box" + task.id}>{task.text}</label>
-        </li>
+      .map((task) => (
+        <ListItem key={task.id} dense button>
+          <ListItemIcon>
+            <Checkbox
+              edge="start"
+              checked={task.completed}
+              onChange={taskDone}
+              name={task.id}
+            />
+          </ListItemIcon>
+          <ListItemText primary={task.text} />
+          <ListItemSecondaryAction>
+            <IconButton edge="end" aria-label="comments">
+              <CommentIcon />
+            </IconButton>
+          </ListItemSecondaryAction>
+        </ListItem>
       ));
   };
 
+  // const CompletedTasks = () => {
+  //   return todoList
+  //     .filter(task => task.completed === true)
+  //     .sort((a, b) => b.date - a.date)
+  //     .map(task => (
+  //       <li key={task.id}>
+  //         <input
+  //           type="checkbox"
+  //           key={task.id}
+  //           name={task.id}
+  //           checked={task.completed}
+  //           onChange={taskDone}
+  //           id={"box" + task.id}
+  //         />
+  //         <label htmlFor={"box" + task.id}>{task.text}</label>
+  //       </li>
+  //     ));
+  // };
   const CompletedTasks = () => {
     return todoList
-      .filter(task => task.completed === true)
+      .filter((task) => task.completed === true)
       .sort((a, b) => b.date - a.date)
-      .map(task => (
-        <li key={task.id}>
-          <input
-            type="checkbox"
-            key={task.id}
-            name={task.id}
-            checked={task.completed}
-            onChange={taskDone}
-            id={"box" + task.id}
-          />
-          <label htmlFor={"box" + task.id}>{task.text}</label>
-        </li>
+      .map((task) => (
+        <ListItem key={task.id} dense button>
+          <ListItemIcon>
+            <Checkbox
+              edge="start"
+              checked={task.completed}
+              onChange={taskDone}
+              name={task.id}
+            />
+          </ListItemIcon>
+          <ListItemText primary={task.text} />
+          <ListItemSecondaryAction>
+            <IconButton edge="end" aria-label="comments">
+              <CommentIcon />
+            </IconButton>
+          </ListItemSecondaryAction>
+        </ListItem>
       ));
   };
 
   const ActiveCounter = () => {
-    const completedAmount = todoList.filter(todo => todo.completed === false)
+    const completedAmount = todoList.filter((todo) => todo.completed === false)
       .length;
     if (completedAmount === 1) {
       return (
-        <h5>
-          {" "}
-          <span>{completedAmount}</span> active task remains.
-        </h5>
+        <Typography variant="h6" gutterBottom>
+          <Box textAlign="left" m={1}>
+            {completedAmount} active task remains.
+          </Box>
+        </Typography>
       );
     }
     return (
-      <h5>
-        {" "}
-        <span>{completedAmount}</span> active tasks remain.
-      </h5>
+      <Typography variant="h6" gutterBottom>
+        <Box textAlign="left" m={1}>
+          {completedAmount} active tasks remain.
+        </Box>
+      </Typography>
     );
   };
 
   const CompletedCounter = () => {
-    const completedAmount = todoList.filter(todo => todo.completed === true)
+    const completedAmount = todoList.filter((todo) => todo.completed === true)
       .length;
     if (completedAmount === 1) {
       return (
-        <h5>
-          {" "}
-          <span>{completedAmount}</span> task completed.
-        </h5>
+        <Typography variant="h6" gutterBottom>
+          <Box textAlign="left" m={1}>
+            {completedAmount} task completed
+          </Box>
+        </Typography>
       );
     }
 
     return (
-      <h5>
-        {" "}
-        <span>{completedAmount}</span> tasks completed.
-      </h5>
+      <Typography variant="h6" gutterBottom>
+        <Box textAlign="left" m={1}>
+          {completedAmount} tasks completed.
+        </Box>
+      </Typography>
     );
   };
 
@@ -150,27 +217,69 @@ function App() {
 
         <section className="todo-wrapper">
           <form onSubmit={addTodo}>
-            <div className="input-wrapper">
-              <input
-                type="text"
-                placeholder="Type your task here..."
-                value={newTodo}
-                onChange={onChangeHandler}
-              />
-              <button type="submit">ADD</button>
-            </div>
-            <div className="todo-list">
-              <ul className="active">
-                <ActiveCounter />
+            <Grid container>
+              <Grid item xs={10}>
+                <TextField
+                  id="outlined-basic"
+                  label="Type your task here..."
+                  variant="outlined"
+                  value={newTodo}
+                  onChange={onChangeHandler}
+                  fullWidth
+                  size="small"
+                  color="secondary"
+                />
+              </Grid>
+              <Grid item xs={2}>
+                <Button
+                  variant="contained"
+                  color="secondary"
+                  type="submit"
+                  size="small"
+                  className={classes.button}
+                >
+                  <AddIcon style={{ fontSize: 30 }} />
+                </Button>
+              </Grid>
+            </Grid>
+            <div className="input-wrapper"></div>
 
+            <div className="todo-list">
+              <ExpansionPanel>
+                <ExpansionPanelSummary
+                  expandIcon={<ExpandMoreIcon />}
+                  aria-label="Expand"
+                  aria-controls="additional-actions1-content"
+                  id="additional-actions1-header"
+                >
+                  <FormControlLabel
+                    aria-label="Acknowledge"
+                    onClick={(event) => event.stopPropagation()}
+                    onFocus={(event) => event.stopPropagation()}
+                    control={<Checkbox />}
+                    label="I acknowledge that I should stop the click event propagation"
+                  />
+                </ExpansionPanelSummary>
+                <ExpansionPanelDetails>
+                  <Typography color="textSecondary">
+                    The click event of the nested action will propagate up and
+                    expand the panel unless you explicitly stop it.
+                  </Typography>
+                </ExpansionPanelDetails>
+              </ExpansionPanel>
+
+
+
+              <ActiveCounter />
+              <List className={classes.root}>
                 <ActiveTasks />
-              </ul>
+              </List>
             </div>
             <div className="todo-list">
-              <ul className="active">
+              <List className={classes.root}>
                 <CompletedCounter />
                 <CompletedTasks />
-              </ul>
+              </List>
             </div>
           </form>
         </section>
